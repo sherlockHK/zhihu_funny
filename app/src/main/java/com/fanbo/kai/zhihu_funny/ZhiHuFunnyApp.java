8c1268128a5670de49d932bed14a0945da5fbc5d
Kai <576717731@qq.com>
2017-01-20 15:27:21 +0800

    1，引入butterknife，dagger2，友盟，rxjava，retrofit2，,support7，design等库； 2，mvp结构目录

---
+++ app/src/main/java/com/fanbo/kai/zhihu_funny/ZhiHuFunnyApp.java
@@ -0,0 +1,14 @@
+package com.fanbo.kai.zhihu_funny;
+
+import android.app.Application;
+
+/**
+ * Created by Kai on 2017/1/20.
+ */
+
+public class ZhiHuFunnyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
